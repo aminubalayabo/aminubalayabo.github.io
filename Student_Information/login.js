a41b5ffce66d61580240eca9228debb9e3ea42a0
aminubalayabo <149179176+aminubalayabo@users.noreply.github.com>
2024-09-14 11:36:34 +0100

    Create login.js

---
+++ Student_Information/login.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    $('#loginForm').submit(function(e) {
+        e.preventDefault();
+
+        var formData = $(this).serialize();
+
+        $.ajax({
+            url: 'login.php',
+            type: 'POST',
+            data: formData,
+            success: function(response) {
+                if (response === 'Login successful') {
+                    window.location.href = 'profile.php?username=' + $('#username').val();
+                } else {
+                    alert(response);
+                }
+            },
+            error: function() {
+                alert('An error occurred during login.');
+            }
+        });
+    });
+});
+Last edited 4 minutes ago
